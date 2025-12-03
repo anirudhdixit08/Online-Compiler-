4a2ef43eaed6f2f04b3b2e7b4f49cb16963cf80e
Anirudh Dixit <anirudhdixit.sjknp@gmail.com>
2025-12-03 15:35:22 +0530

    Basic compiler completed:

--- backend/controllers/executeJs.js
+++ backend/controllers/executeJs.js
@@ -1,44 +1,45 @@
 import { spawn } from "child_process";
 import fs from "fs";
-import path from "path";
 
 const executeJs = (filePath, inputFilePath) => {
   return new Promise((resolve, reject) => {
     try {
-      console.log("Running JS file:", filePath);
+      setTimeout(() => {
+        console.log("Running JS file:", filePath);
 
-      const run = spawn("node", [filePath], { shell: false });
+        const run = spawn("node", [filePath], { shell: false });
 
-      let stdout = "";
-      let stderr = "";
+        let stdout = "";
+        let stderr = "";
 
-      if (inputFilePath) {
-        try {
-          const inputStream = fs.createReadStream(inputFilePath);
-          inputStream.pipe(run.stdin);
-        } catch (err) {
-          return reject({ error: "Input file error", stderr: err });
+        if (inputFilePath) {
+          try {
+            const inputStream = fs.createReadStream(inputFilePath);
+            inputStream.pipe(run.stdin);
+          } catch (err) {
+            return reject({ error: "Input file error", stderr: err });
+          }
         }
-      }
 
-      run.stdout.on("data", (data) => {
-        stdout += data.toString();
-      });
+        run.stdout.on("data", (data) => {
+          stdout += data.toString();
+        });
 
-      run.stderr.on("data", (data) => {
-        stderr += data.toString();
-      });
+        run.stderr.on("data", (data) => {
+          stderr += data.toString();
+        });
 
-      run.on("error", (err) => {
-        return reject({ error: "Node spawn error", stderr: err });
-      });
+        run.on("error", (err) => {
+          return reject({ error: "Node spawn error", stderr: err });
+        });
 
-      run.on("close", (code) => {
-        if (code !== 0) {
-          return reject({ error: "Runtime error", stderr });
-        }
-        resolve(stdout);
-      });
+        run.on("close", (code) => {
+          if (code !== 0) {
+            return reject({ error: "Runtime error", stderr });
+          }
+          resolve(stdout);
+        });
+      }, 500);
     } catch (syncErr) {
       reject({ error: "Unexpected error", stderr: syncErr });
     }
